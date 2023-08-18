e44fd86a4ab6693f5ee169327611ebed593c153a
Lachlan D'Arcy <darcylachlan@hotmail.com.au>
2023-08-18 14:47:25 +1000

    fix(*) WIP addedd placeholder text to projects and other sections

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // Types
 import type { NextPage } from 'next';
+// Functions
+import clsx from 'clsx';
 // Components
 import Head from 'next/head';
 import Button from '../components/reusable/button/button';
@@ -8,12 +10,14 @@ import Input from '../components/reusable/input/input';
 import Container from '../components/reusable/container/container';
 import Header from '../components/header/header';
 import Project from '../components/project/project';
+import Divider from '../components/reusable/divider/divider';
+import Nav from '../components/nav/nav';
 // Constants
 import builtWithTechologies from '../constants/builtWithTechnologies';
 // Assets
 // Styles
 import classes from './index.module.scss';
-import Divider from '../components/reusable/divider/divider';
+import textClasses from '../styles/classes/text.module.scss';
 
 const Home: NextPage = () => {
   return (
@@ -23,67 +27,94 @@ const Home: NextPage = () => {
         <meta name='description' content='' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Header />
-      <Container>
-        <h2>Personal Projects</h2>
-        <Divider />
-        <Project
-          project={{
-            title: 'Your Chances',
-            description: `One of my first projects. Using data from the CIA World Factbook, 
-            it gives you the chances of being born in a certain country. It was originally built in Vanilla JS, CSS and HTML. 
-            But after becoming more experienced I remade it with React, and replaced CSS with Styled Componentss. 
-            Then after a lot of time using React-JSS I made the switch from styled components to React-JSS and used my own theme component Themerig`,
-            links: {
-              demo: '/',
-              github: '/',
-            },
-            builtWithTechnologies: [builtWithTechologies['javascript']],
-          }}
-        />
-        <Project
-          project={{
-            title: 'Simple Typing Test',
-            description: `After using React for a while, I wanted to create something with vanilla Javascript again, so I created a simple typing test. 
-            It made me appreciate how easy it is to build web apps with React.`,
-            links: {
-              demo: '/',
-              github: '/',
-            },
-            builtWithTechnologies: [builtWithTechologies['javascript'], builtWithTechologies['css'], builtWithTechologies['html']],
-          }}
-        />
-        <Project
-          project={{
-            title: 'Portfolio',
-            description: `This website, which went through a lot of revisions to get where it is now. 
-            Sadly design has never come as easily to me as other aspects of web development, even though I have a passion for it. 
-            Although this has led me to wanting to find systems to define design and started my research into Design Systems.`,
-            links: {
-              demo: '/',
-              github: '/',
-            },
-            builtWithTechnologies: [
-              builtWithTechologies['javascript'],
-              builtWithTechologies['react'],
-              builtWithTechologies['nextJs'],
-              builtWithTechologies['jss'],
-            ],
-          }}
-        />
-      </Container>
-      <Container>
-        <h2>My stack</h2>
-      </Container>
-      <Container>
-        <h2>About me</h2>
-      </Container>
-      <Container>
-        <h2>Get in Touch</h2>
-      </Container>
-      <footer>
-        <Container>footer</Container>
-      </footer>
+      <div className={classes['root']}>
+        <Nav />
+        <main className={classes['main']}>
+          {/* <h1 className={clsx(classes['title'], textClasses['title-text'])}>Lachlan D'Arcy</h1> */}
+          <section className={classes['section']}>
+            <h2 className={classes['heading']} id='technologies'>
+              Technologies
+            </h2>
+            <p>
+              I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+              in an interactive and collaborative environment to develop quality websites.
+            </p>
+          </section>
+          <section className={classes['section']}>
+            <h2 className={classes['heading']} id='work-experience'>
+              Work experience
+            </h2>
+            <p>
+              I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+              in an interactive and collaborative environment to develop quality websites.
+            </p>
+          </section>
+          <section className={classes['section']}>
+            <h2 className={classes['heading']} id='creative-projects'>
+              Creative projects
+            </h2>
+            <Project
+              project={{
+                title: 'Your Chances',
+                description: `I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+                in an interactive and collaborative environment to develop quality websites.`,
+                links: {
+                  demo: '/',
+                  github: '/',
+                },
+                builtWithTechnologies: [builtWithTechologies['javascript']],
+              }}
+            />
+            <Project
+              project={{
+                title: 'Simple Typing Test',
+                description: `I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+                in an interactive and collaborative environment to develop quality websites.`,
+                links: {
+                  demo: '/',
+                  github: '/',
+                },
+                builtWithTechnologies: [builtWithTechologies['javascript'], builtWithTechologies['css'], builtWithTechologies['html']],
+              }}
+            />
+            <Project
+              project={{
+                title: 'Portfolio',
+                description: `I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+                in an interactive and collaborative environment to develop quality websites.`,
+                links: {
+                  demo: '/',
+                  github: '/',
+                },
+                builtWithTechnologies: [
+                  builtWithTechologies['javascript'],
+                  builtWithTechologies['react'],
+                  builtWithTechologies['nextJs'],
+                  builtWithTechologies['jss'],
+                ],
+              }}
+            />
+          </section>
+          <section className={classes['section']}>
+            <h2 className={classes['heading']} id='about-me'>
+              About me
+            </h2>
+            <p>
+              I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+              in an interactive and collaborative environment to develop quality websites.
+            </p>
+          </section>
+          <section className={classes['section']}>
+            <h2 className={classes['heading']} id='contact-me'>
+              Contact me
+            </h2>
+            <p>
+              I’m a passionate Full-Stack Web Developer looking for work as a full-stack or front-end developer. I’m looking forward to expanding my knowledge
+              in an interactive and collaborative environment to develop quality websites.
+            </p>
+          </section>
+        </main>
+      </div>
     </>
   );
 };
