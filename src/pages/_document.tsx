57e63a6400c6df3836b115f789653c952e294e3c
Lachlan D'Arcy <darcylachlan@hotmail.com.au>
2023-08-18 14:44:04 +1000

    feat(*) added IBM plex sans font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,12 @@ class _Document extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link href='https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap' rel='stylesheet' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&family=Inter:wght@300;400;500;700;800&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
