b2cc33e4d1555ee3eae48d6a269480961ba718ef
Lachlan D'Arcy <darcylachlan@hotmail.com.au>
2023-11-27 15:26:40 +1000

    fix(*) commented out currently undeployed projects

--- src/components/pages/home/sections/creativeProjects/creativeProjects.tsx
+++ src/components/pages/home/sections/creativeProjects/creativeProjects.tsx
@@ -18,28 +18,31 @@ const CreativeProjects = forwardRef((props: TCreativeProjectsProps, ref: Forward
         Creative Projects
       </h2>
 
-      <div className={styles['projects']}>
-        <Project
+      {/* <Project
           project={{
             title: 'Game of Ur',
             description: `
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et nunc tempor, eleifend leo ac, maximus elit. Donec rutrum, nulla at varius
-              venenatis, nisl sapien aliquet leo, venenatis viverra eros mauris eget turpis.`,
+              A web application for the Royal Game of Ur. Built because I wanted to work with socket.io and I love the history of the game.`,
             links: { demo: '', github: '' },
-            builtWithTechnologies: [builtWithTechnologies['react']],
+            builtWithTechnologies: [
+              builtWithTechnologies['nodeJs'],
+              builtWithTechnologies['socket'],
+              builtWithTechnologies['react'],
+              builtWithTechnologies['sass'],
+              builtWithTechnologies['webpack'],
+            ],
           }}
-        />
-        <Project
-          project={{
-            title: 'Timeflit',
-            description: `
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et nunc tempor, eleifend leo ac, maximus elit. Donec rutrum, nulla at varius
-              venenatis, nisl sapien aliquet leo, venenatis viverra eros mauris eget turpis.`,
-            links: { demo: '', github: '' },
-            builtWithTechnologies: [builtWithTechnologies['react']],
-          }}
-        />
-        <Project
+        /> */}
+      <Project
+        project={{
+          title: 'Timeflit',
+          description: `
+              A todo web app that allows you to both make and track time estimates for tasks. Built because I wanted a way to track my time estimates.`,
+          links: { demo: '', github: '' },
+          builtWithTechnologies: [builtWithTechnologies['react'], builtWithTechnologies['sass'], builtWithTechnologies['webpack']],
+        }}
+      />
+      {/* <Project
           project={{
             title: 'Your chances',
             description: `
@@ -48,28 +51,25 @@ const CreativeProjects = forwardRef((props: TCreativeProjectsProps, ref: Forward
             links: { demo: '', github: '' },
             builtWithTechnologies: [builtWithTechnologies['react']],
           }}
-        />
-        <Project
+        /> */}
+      {/* <Project
           project={{
             title: 'Simple Writing Test',
             description: `
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et nunc tempor, eleifend leo ac, maximus elit. Donec rutrum, nulla at varius
-              venenatis, nisl sapien aliquet leo, venenatis viverra eros mauris eget turpis.`,
+              A writing speed test website. Built because I wanted to make an application with vanilla javascript after using react for so long.`,
             links: { demo: '', github: '' },
-            builtWithTechnologies: [builtWithTechnologies['react']],
+            builtWithTechnologies: [builtWithTechnologies['html'], builtWithTechnologies['javascript']],
           }}
-        />
-        <Project
-          project={{
-            title: 'Portfolio Site',
-            description: `
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et nunc tempor, eleifend leo ac, maximus elit. Donec rutrum, nulla at varius
-              venenatis, nisl sapien aliquet leo, venenatis viverra eros mauris eget turpis.`,
-            links: { demo: '', github: '' },
-            builtWithTechnologies: [builtWithTechnologies['react']],
-          }}
-        />
-      </div>
+        /> */}
+      <Project
+        project={{
+          title: 'Portfolio Site',
+          description: `
+              A website to display what I've worked on and where you can find me.`,
+          links: { demo: 'lachlandarcy.com', github: '' },
+          builtWithTechnologies: [builtWithTechnologies['react']],
+        }}
+      />
     </section>
   );
 });
