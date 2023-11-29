1b048383b67e1d913a39a3932b01a337d57c6363
Lachlan D'Arcy <darcylachlan@hotmail.com.au>
2023-11-30 09:51:38 +1000

    feat(*) replaced placeholder text in contact me with actual text feat(*) changed icons used in contact me section feat(*) trialing adding --icon-color css var in svg icons for easier color changing

--- src/components/pages/home/sections/contactMe/contactMe.tsx
+++ src/components/pages/home/sections/contactMe/contactMe.tsx
@@ -4,6 +4,11 @@ import clsx from 'clsx';
 // Styles
 import textStyles from '../../../../../styles/text.module.scss';
 import styles from './contactMe.module.scss';
+import Button from '../../../../reusable/button/button';
+import { GithubIcon } from '../../../../../static/icons/technologies';
+import LinkedInIcon from '../../../../../static/icons/socials/linkedInIcon';
+import MailIcon from '../../../../../static/icons/socials/mailIcon';
+import Tag from '../../../../reusable/tag/tag';
 
 type Props = {};
 
@@ -13,10 +18,27 @@ const ContactMe = forwardRef((props: Props, ref: ForwardedRef<HTMLElement>) => {
       <h2 id='contact-me' className={clsx(textStyles['heading-text'], styles['heading'])}>
         Contact me
       </h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et nunc tempor, eleifend leo ac, maximus elit. Donec rutrum, nulla at varius venenatis, nisl
-        sapien aliquet leo, venenatis viverra eros mauris eget turpis.
-      </p>
+      <p className={styles['paragraph']}>If you&apos;re interested in working together, consider contacting me through one of these platforms!</p>
+      <div className={styles['tag-container']}>
+        <a href='mailto:darcylachlan@hotmail.com.au' target='_blank' rel='noreferrer' className={styles['tag-wrapper']}>
+          <Tag className={styles['tag']}>
+            <MailIcon />
+            Email
+          </Tag>
+        </a>
+        <a href='https://www.linkedin.com/in/lachlan-d-arcy-161241208/' target='_blank' rel='noreferrer' className={styles['tag-wrapper']}>
+          <Tag className={styles['tag']}>
+            <LinkedInIcon />
+            LinkedIn
+          </Tag>
+        </a>
+        <a href='https://github.com/aDarcy99' target='_blank' rel='noreferrer' className={styles['tag-wrapper']}>
+          <Tag className={styles['tag']}>
+            <GithubIcon />
+            Github
+          </Tag>
+        </a>
+      </div>
     </section>
   );
 });
